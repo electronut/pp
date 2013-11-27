b1f984e9fd7ac63fad99060bdd48e70edac29e71
electronut <info@electronut.in>
2013-11-27 15:02:30 +0530

    data set improved

--- volrender/makedata.py
+++ volrender/makedata.py
@@ -22,19 +22,21 @@ def makeSphereCuboid():
             for i in range(Nx):
                 # inside cuboid?
                 if abs(i-Nx2) < 40 and abs(j-Ny2) < 30 and abs(k-Nz2) < 20:
-                    a[i][j][k] = 200
+                    a[i][j][k] = min(2*(-Nx2 + 40 + i), 255)
                 else:
                     # inside sphere?
                     if ((i-Nx2)*(i-Nx2) + (j-Ny2)*(j-Ny2) + 
                         (k-Nz2)*(k-Nz2)) < 100*100:
-                        a[i][j][k] = 100
+                        a[i][j][k] = 50
     # create dir if not exists                
     imgDir = 'sphere-cuboid'
     if not os.path.exists(imgDir):
         os.makedirs(imgDir)
     # write images
     for i in range(Nz):
+        # create image from data
         im = Image.fromarray(a[i])
+        # save file
         fileName = os.path.join(imgDir, 'sphere-cuboid%03d.png' % i)
         im.save(fileName)
 
