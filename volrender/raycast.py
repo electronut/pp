6a2c0c2f285e287f02da4138d66bc81e6a7666db
electronut <info@electronut.in>
2013-11-30 20:29:54 +0530

    cleanup of fragment shader

--- volrender/raycast.py
+++ volrender/raycast.py
@@ -52,34 +52,48 @@ void main()
 {
     // start of ray
     vec3 start = vColor.rgb;
+
     // calculate texture coords at fragment, which is a 
     // fraction of window coords
     vec2 texc = gl_FragCoord.xy/uWinDims.xy;
-    // get back face color
-    vec4 colBackFace = texture(texBackFaces, texc);
+
+    // get end of ray = back-face color
+    vec3 end = texture(texBackFaces, texc).rgb;
+
     // calculate ray direction
-    vec3 dir = colBackFace.rgb - start;
+    vec3 dir = end - start;
+
     // normalized ray direction
     vec3 norm_dir = normalize(dir);
+
     // the length from front to back is calculated and 
     // used to terminate the ray
     float len = length(dir.xyz);
+
     // ray step size
     float stepSize = 0.01;
 
     // X-Ray projection
     vec4 dst = vec4(0.0);
        
+    // step through the ray
     for(float t = 0.0; t < len; t += stepSize) {
-        // end point of ray
+
+        // set position to end point of ray
         vec3 samplePos = start + t*norm_dir;
-        float value = texture(texVolume, samplePos).r;
-        vec4 src = vec4(value);
+
+        // get texture value at position
+        float val = texture(texVolume, samplePos).r;
+        vec4 src = vec4(val);
+
+        // set opacity
         src.a *= 0.1; 
         src.rgb *= src.a;
+
+        // blend with previous value
         dst = (1.0 - dst.a)*src + dst;		
             
-        // break when alpha is high enough
+        // exit loop when alpha exceeds threshold
         if(dst.a >= 0.95)
             break;
     }
