b683bc0dbfda80374642c9ce02e2858df982cd1d
electronut <info@electronut.in>
2014-03-11 12:14:25 +0530

    converted to python3

--- conway/conway.py
+++ conway/conway.py
@@ -63,10 +63,10 @@ def update(frameNum, img, grid, N):
             # compute 8-neghbor sum
             # using toroidal boundary conditions - x and y wrap around 
             # so that the simulaton takes place on a toroidal surface.
-            total = (grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
-                     grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
-                     grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
-                     grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
             # apply Conway's rules
             if grid[i, j]  == ON:
                 if (total < 2) or (total > 3):
