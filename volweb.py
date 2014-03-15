512bbab6a6774b19726cb87a2712aff8787cf353
electronut <info@electronut.in>
2014-03-15 10:30:05 +0530

    added a GET method

--- volweb.py
+++ volweb.py
@@ -1,4 +1,4 @@
-from bottle import route, run, response
+from bottle import route, run, response, request
 from PIL import Image, ImageDraw
 import random
 import io
@@ -12,8 +12,27 @@ def hello():
 def test_template(): 
     return '<h4>Random Tile </h4><img src="/img" />';
 
-@route('/img')
+@route('/img', method='GET')
 def img():
+    # read query values
+    vals = {'w':100, 'h':100, 'r':255, 'g':0, 'b':0}
+    for key in vals.keys():
+        tmp = request.query.get(key)
+        if tmp:
+            vals[key] = int(tmp)
+    # set response
+    response.content_type = 'image/png'
+    # create image
+    im = Image.new('RGB', (vals['w'], vals['h']), 
+                   (vals['r'], vals['g'], vals['b']))
+    # send as bytes
+    strData = io.BytesIO()
+    im.save(strData, 'PNG')
+    strData.seek(0)
+    return strData.getvalue()
+
+@route('/rimg')
+def rimg():
     im = createRandomTile((100, 100))
     response.content_type = 'image/png'
     strData = io.BytesIO()
