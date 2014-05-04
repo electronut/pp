02380ef6a5e1258550421833a2cef5b5cf466034
electronut <info@electronut.in>
2014-05-04 08:29:35 +0530

    merged AnalogData class with AnalogPlot

--- arduino-ldr/ldr.py
+++ arduino-ldr/ldr.py
@@ -11,45 +11,53 @@ import sys, serial, argparse
 import numpy as np
 from time import sleep
 from collections import deque
-from matplotlib import pyplot as plt
 
-# class that holds analog data for N samples
-class AnalogData:
-  # constr
-  def __init__(self, maxLen):
-    self.ax = deque([0.0]*maxLen)
-    self.ay = deque([0.0]*maxLen)
-    self.maxLen = maxLen
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
 
-  # ring buffer
-  def addToBuf(self, buf, val):
-    if len(buf) < self.maxLen:
-      buf.append(val)
-    else:
-      buf.pop()
-      buf.appendleft(val)
-
-  # add data
-  def add(self, data):
-    assert(len(data) == 2)
-    self.addToBuf(self.ax, data[0])
-    self.addToBuf(self.ay, data[1])
     
 # plot class
 class AnalogPlot:
   # constr
-  def __init__(self, analogData):
-    # set plot to animated
-    plt.ion() 
-    self.axline, = plt.plot(analogData.ax)
-    self.ayline, = plt.plot(analogData.ay)
-    plt.ylim([0, 1023])
+  def __init__(self, strPort, maxLen):
+      # open serial port
+      self.ser = serial.Serial(strPort, 9600)
+
+      self.ax = deque([0.0]*maxLen)
+      self.ay = deque([0.0]*maxLen)
+      self.maxLen = maxLen
+
+      # set plot to animated
+      plt.ion() 
+      self.axline, = plt.plot(self.ax)
+      self.ayline, = plt.plot(self.ay)
+      plt.ylim([0, 1023])
+
+  # add to buffer
+  def addToBuf(self, buf, val):
+      if len(buf) < self.maxLen:
+          buf.append(val)
+      else:
+          buf.pop()
+          buf.appendleft(val)
+
+  # add data
+  def add(self, data):
+      assert(len(data) == 2)
+      self.addToBuf(self.ax, data[0])
+      self.addToBuf(self.ay, data[1])
 
   # update plot
-  def update(self, analogData):
-    self.axline.set_ydata(analogData.ax)
-    self.ayline.set_ydata(analogData.ay)
-    plt.draw()
+  def update(self):
+      self.axline.set_ydata(self.ax)
+      self.ayline.set_ydata(self.ay)
+      plt.draw()
+
+  # clean up
+  def close(self):
+      # close serial
+      self.ser.flush()
+      self.ser.close()    
 
 # main() function
 def main():
@@ -67,27 +75,25 @@ def main():
   print('reading from serial port %s...' % strPort)
 
   # plot parameters
-  analogData = AnalogData(100)
-  analogPlot = AnalogPlot(analogData)
+  analogPlot = AnalogPlot(strPort, 100)
 
   print('plotting data...')
 
-  # open serial port
-  ser = serial.Serial(strPort, 9600)
+  ser = analogPlot.ser
   while True:
     try:
       line = ser.readline()
       data = [float(val) for val in line.split()]
       #print data
       if(len(data) == 2):
-        analogData.add(data)
-        analogPlot.update(analogData)
+        analogPlot.add(data)
+        analogPlot.update()
     except KeyboardInterrupt:
       print('exiting')
       break
-  # close serial
-  ser.flush()
-  ser.close()
+  
+  # clean up
+  analogPlot.close()
 
 # call main
 if __name__ == '__main__':
