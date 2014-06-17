689f213be6d3f8f2171aa2de35f8dedae9b45e1c
electronut <info@electronut.in>
2014-06-17 12:02:17 +0530

    minor fixes

--- piweather/piweather.py
+++ piweather/piweather.py
@@ -10,7 +10,6 @@ import sys
 import wiringpi2 as wiringpi
 from time import sleep  
 
-
 def blink():
     """
     Make LED connected to pin 18 (board) blink
@@ -54,21 +53,74 @@ def getDHT11Data(pinNum):
         val = wiringpi.digitalRead(pinNum)
         samples.append(val)
         # sample every 20 microseconds
-        #wiringpi.delayMicroseconds(srate)
+        #wiringpi.delayMicroseconds(srate)    
+    return samples
+
+def decodeDH11Data(data):
+    """
+    decode DH11 data from string.
+    """
+    # store previous value
+    prev = 0
+    # store count to keep tack of oulse width
+    count = 0
+    # store decoded string here
+    bits = ''
+    # iterate through data
+    for i in range(len(data)):
+        # low to high transition
+        if prev == 0 and data[i] == 1:
+            # reset counter
+            count = 1
+        # high to low transition
+        elif prev == 1 and data[i] == 0:
+            # detect 1 vs 0
+            if count > 3:
+                bits += '1'
+            else:
+                bits += '0'
+        # increment count
+        else:
+            count += 1
+        # update prev value
+        prev = data[i]
+
+    print len(bits)
+    print bits
+
+    if len(bits) == 40:
+        # convert string to integers
+        x = []
+        for i in range(0, 40, 8):
+            x.append(int(bits[i:i+8], 2))
+        # verify checksum
+        if (sum(x[:4]) == x[4]):
+            # print('checksum verified')
+            # convert to float strings
+            strRH = str(x[0]) + '.' + str(x[1])
+            strT = str(x[2]) + '.' + str(x[3])
+            return (strRH, strT)
     
-    f = open('samples.txt', 'w')
-    for i in range(len(samples)):
-        f.write("%d %d\n" % (i, samples[i]))
-    f.close()
-    print('done')
+    return None
 
 # main() function
 def main():
     # use sys.argv if needed
     print 'starting piweather...'
     
-    getDHT11Data(16)
-
+    """
+    f = open('samples.txt', 'w')
+    for i in range(len(samples)):
+        f.write("%d %d\n" % (i, samples[i]))
+    f.close()
+    print('done')
+    """
+    while True:
+        data = getDHT11Data(16)
+        d = decodeDH11Data(data)
+        if d:
+            print("RH = %s, T = %s" % (d[0], d[1]))
+        sleep(2)
 # call main
 if __name__ == '__main__':
     main()
