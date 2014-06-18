045948dffccb3771163cf8d0a79c7965afc77b60
electronut <info@electronut.in>
2014-06-18 09:12:23 +0530

    clean up

--- piweather/test.py
+++ piweather/test.py
@@ -1,6 +1,39 @@
 import numpy as np
 from matplotlib import pyplot
 
+def getDHT11Data(pinNum):
+    """
+    Get data from DHT11 conencted to pinNum (board)
+    """
+    
+    # set pin as output
+    wiringpi.wiringPiSetupPhys() 
+    wiringpi.pinMode(pinNum, 1)
+
+    # pull down for 20 ms = 20000 us
+    wiringpi.digitalWrite(pinNum, 0) 
+    wiringpi.delayMicroseconds(20000)
+    # pull up
+    wiringpi.digitalWrite(pinNum, 1) 
+
+    # set pin as input
+    wiringpi.pinMode(pinNum, 0)
+    # wait for 20us
+    wiringpi.delayMicroseconds(18)
+
+    # read data for t = (50 + 70) * 40 + 160 us
+    # ~ 5 ms = 5000 us    
+    srate = 10
+    N = 5000//srate
+    samples = numpy.zeros(N, numpy.int16)
+    for i in range(N):
+        # read pin
+        val = wiringpi.digitalRead(pinNum)
+        samples[i] = val
+        # sample every 20 microseconds
+        #wiringpi.delayMicroseconds(1)    
+
+    return samples
 
 def decodeDH11Data(data):
     """
@@ -31,21 +64,24 @@ def decodeDH11Data(data):
         # update prev value
         prev = data[i]
 
-    #print len(bits)
-    #print bits
+    print len(bits)
+    print bits
 
-    # convert string to integers
-    data = []
-    for i in range(0, 40, 8):
-        data.append(int(bits[i:i+8], 2))
-    # verify checksum
-    if (sum(data[:4]) == data[4]):
-        # print('checksum verified')
-        # convert to float strings
-        strRH = str(data[0]) + '.' + str(data[1])
-        strT = str(data[2]) + '.' + str(data[3])
-        return (strRH, strT)
+    if len(bits) == 40:
+        # convert string to integers
+        x = []
+        for i in range(0, 40, 8):
+            x.append(int(bits[i:i+8], 2))
+        # verify checksum
+        if (sum(x[:4]) == x[4]):
+            # print('checksum verified')
+            # convert to float strings
+            strRH = str(x[0]) + '.' + str(x[1])
+            strT = str(x[2]) + '.' + str(x[3])
+            return (strRH, strT)
+    
     return None
+
 
 f = open('samples.txt')
 vals = [int(i) for i in f.read().split()]
