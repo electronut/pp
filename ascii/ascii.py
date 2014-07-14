db693c3e9cc13bf629e268baa00faf471e89797c
electronut <info@electronut.in>
2014-07-14 13:49:57 +0530

    added help, fixed formatting

--- ascii/ascii.py
+++ ascii/ascii.py
@@ -87,47 +87,46 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
 
 # main() function
 def main():
-  # use sys.argv if needed
-  print('generating ASCII art...')
-  # create parser
-  parser = argparse.ArgumentParser(description="ASCI Art...")
-  # add expected arguments
-  parser.add_argument('--file', dest='imgFile', required=True)
-  parser.add_argument('--scale', dest='scale', required=False)
-  parser.add_argument('--out', dest='outFile', required=False)
-  parser.add_argument('--cols', dest='cols', required=False)
-  parser.add_argument('--morelevels',dest='moreLevels',action='store_true')
+    # create parser
+    descStr = "This program converts an image into ASCII art."
+    parser = argparse.ArgumentParser(description=descStr)
+    # add expected arguments
+    parser.add_argument('--file', dest='imgFile', required=True)
+    parser.add_argument('--scale', dest='scale', required=False)
+    parser.add_argument('--out', dest='outFile', required=False)
+    parser.add_argument('--cols', dest='cols', required=False)
+    parser.add_argument('--morelevels',dest='moreLevels',action='store_true')
 
-  # parse args
-  args = parser.parse_args()
+    # parse args
+    args = parser.parse_args()
   
-  imgFile = args.imgFile
-  # set output file
-  outFile = 'out.txt'
-  if args.outFile:
-      outFile = args.outFile
-  # set scale default as 0.43 which suits a Courier font
-  scale = 0.43
-  if args.scale:
-      scale = float(args.scale)
-  # set cols
-  cols = 80
-  if args.cols:
-      cols = int(args.cols)
+    imgFile = args.imgFile
+    # set output file
+    outFile = 'out.txt'
+    if args.outFile:
+        outFile = args.outFile
+    # set scale default as 0.43 which suits a Courier font
+    scale = 0.43
+    if args.scale:
+        scale = float(args.scale)
+    # set cols
+    cols = 80
+    if args.cols:
+        cols = int(args.cols)
 
-  # convert image to ascii txt
-  aimg = covertImageToAscii(imgFile, cols, scale, args.moreLevels)
+    print('generating ASCII art...')
+    # convert image to ascii txt
+    aimg = covertImageToAscii(imgFile, cols, scale, args.moreLevels)
 
-  # open file
-  f = open(outFile, 'w')
+    # open file
+    f = open(outFile, 'w')
     # write to file
-  for row in aimg:
-      f.write(row + '\n')
-  # cleanup
-  f.close()
-
-  print("ASCII art written to %s" % outFile)
+    for row in aimg:
+        f.write(row + '\n')
+    # cleanup
+    f.close()
+    print("ASCII art written to %s" % outFile)
 
 # call main
 if __name__ == '__main__':
-  main()
+    main()
