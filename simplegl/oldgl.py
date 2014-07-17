15a99726dab76c404f5e269432344fbff5ea181b
electronut <info@electronut.in>
2014-07-17 07:13:20 +0530

    added a simple old GL example

---
+++ simplegl/oldgl.py
@@ -0,0 +1,32 @@
+"""
+oldgl.py
+
+A demonstration of the now deprecated fixed function pipeline OpenGL API.
+
+Author: Mahesh Venkitachalam
+Website: electronut.in
+
+"""
+
+import sys
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def display():
+   glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+   glColor3f (1.0, 1.0, 0.0)
+   glBegin(GL_QUADS)
+   glVertex3f (-0.5, -0.5, 0.0)
+   glVertex3f (0.5, -0.5, 0.0)
+   glVertex3f (0.5, 0.5, 0.0)
+   glVertex3f (-0.5, 0.5, 0.0)
+   glEnd()
+   glFlush ();
+
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
+glutInitWindowSize(400, 400)
+glutCreateWindow("oldgl")
+glutDisplayFunc(display)
+glutMainLoop()
