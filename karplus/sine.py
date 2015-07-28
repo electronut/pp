f03d33b0bf009a52c70a19acc21e11447ba096e8
electronut <info@electronut.in>
2015-07-28 22:08:38 +0530

    D4 wave corrected

--- karplus/sine.py
+++ karplus/sine.py
@@ -12,9 +12,9 @@ import wave, math
 sRate = 44100
 nSamples = sRate * 5
 x = np.arange(nSamples)/float(sRate)
-vals = np.sin(2.0*math.pi*220.0*x)
+vals = np.sin(2.0*math.pi*146.83*x)
 data = np.array(vals * 32767, 'int16').tostring()
-file = wave.open('sine220.wav', 'wb')
+file = wave.open('sine146_83.wav', 'wb')
 file.setparams((1, 2, sRate, nSamples, 'NONE', 'uncompressed'))
 file.writeframes(data)
 file.close()
